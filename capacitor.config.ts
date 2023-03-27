7eb66b03c499bd546295bd1831f1afd4814e3fbe
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-03-27 14:32:34 +0700

    Capacitor check hostname

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,18 @@
 import { CapacitorConfig } from "@capacitor/cli";
 import { KeyboardResize } from "@capacitor/keyboard";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const config: CapacitorConfig = {
     appId: "io.ionic.starter",
     appName: "base-mobile-app",
     webDir: "dist",
     bundledWebRuntime: false,
+    server: {
+        hostname: process.env.VITE_HOSTNAME,
+    },
     plugins: {
         Keyboard: {
             resize: KeyboardResize.None,
