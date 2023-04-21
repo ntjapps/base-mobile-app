fe0ad1a974e034971b26bbf52668a344ccacac4e
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-04-21 11:32:04 +0700

    Update endpoint

--- src/AppState.ts
+++ src/AppState.ts
@@ -120,31 +120,29 @@ export const useMainStore = defineStore("main", {
 });
 
 export const useEchoStore = defineStore("echo", {
-    getters: {
-        laravelEcho: () => {
-            return new Echo({
-                broadcaster: "pusher",
-                key: import.meta.env.VITE_PUSHER_APP_KEY,
-                cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? "mt1",
-                wsHost: import.meta.env.VITE_PUSHER_HOST
-                    ? import.meta.env.VITE_PUSHER_HOST
-                    : `ws-${
-                          import.meta.env.VITE_PUSHER_APP_CLUSTER
-                      }.pusher.com`,
-                wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
-                wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-                forceTLS:
-                    (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
-                enabledTransports: ["ws", "wss"],
-                //authEndpoint: import.meta.env.VITE_API_ENDPOINT + "/api/broadcasting/auth",
-                //auth: {
-                //headers: {
-                //Accept: "application/json",
-                //},
-                //},
-            });
-        },
-    },
+    state: () => ({
+        laravelEcho: new Echo({
+            broadcaster: "pusher",
+            key: import.meta.env.VITE_PUSHER_APP_KEY,
+            cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? "mt1",
+            wsHost: import.meta.env.VITE_PUSHER_HOST
+                ? import.meta.env.VITE_PUSHER_HOST
+                : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+            wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+            wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+            forceTLS:
+                (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
+            enabledTransports: ["ws", "wss"],
+            authEndpoint:
+                import.meta.env.VITE_API_ENDPOINT + "/api/broadcasting/auth",
+            auth: {
+                headers: {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${useSecureStore().apiToken}`,
+                },
+            },
+        }),
+    }),
 });
 
 export const useSecureStore = defineStore("secure", {
