efd6141b2b83cc7a576e105d4693030b327f07cd
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-09-26 15:08:29 +0700

    Updated Tailwind PT

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { createApp, App } from "vue";
 import { createPinia, Pinia } from "pinia";
 const pinia: Pinia = createPinia();
 import PrimeVue from "primevue/config";
+import { usePassThrough } from "primevue/passthrough";
 import Tailwind from "primevue/passthrough/tailwind";
 
 import { IonicVue } from "@ionic/vue";
@@ -18,12 +19,17 @@ import DialogService from "primevue/dialogservice";
 import ToastService from "primevue/toastservice";
 import Tooltip from "primevue/tooltip";
 
+const CustomTailwind = usePassThrough(Tailwind, {
+    mergeSections: true,
+    mergeProps: false,
+});
+
 // Mount Application Instances
 const MainApp: App<Element> = createApp(BaseApp)
     .use(IonicVue)
     .use(router)
     .use(pinia)
-    .use(PrimeVue, { ripple: true, unstyled: true, pt: Tailwind })
+    .use(PrimeVue, { ripple: true, unstyled: true, pt: CustomTailwind })
     .use(DialogService)
     .use(ToastService)
     .directive("tooltip", Tooltip);
