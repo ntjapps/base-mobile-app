4d4c825f003798e16e9c5d1172dd51d9234487b7
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-12-14 04:21:11 +0000

    Add Sentry for error tracking

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { createPinia, Pinia } from "pinia";
 const pinia: Pinia = createPinia();
 import PrimeVue from "primevue/config";
 import Tailwind from "primevue/passthrough/tailwind";
+import * as Sentry from "@sentry/capacitor";
+import * as SentrySibling from "@sentry/vue";
 
 import { IonicVue } from "@ionic/vue";
 
@@ -17,6 +19,24 @@ import BaseApp from "./BaseApp.vue";
 import DialogService from "primevue/dialogservice";
 import ToastService from "primevue/toastservice";
 import Tooltip from "primevue/tooltip";
+
+/** Sentry iniitialization */
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    release: process.env.npm_package_version,
+    dist: "1",
+    integrations: [
+        new SentrySibling.BrowserTracing({
+            // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+            tracePropagationTargets: ["localhost", import.meta.env.VITE_APP_HOST, import.meta.env.VITE_API_ENDPOINT],
+            routingInstrumentation: SentrySibling.vueRouterInstrumentation(router),
+        }),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    },
+    SentrySibling.init,
+);
 
 // Mount Application Instances
 const MainApp: App<Element> = createApp(BaseApp)
