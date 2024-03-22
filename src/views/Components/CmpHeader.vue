7dd5c7ad0e2fdecdb593b962959171680f49fca5
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-03-22 03:51:56 +0000

    Compact logic browser support

--- src/views/Components/CmpHeader.vue
+++ src/views/Components/CmpHeader.vue
@@ -36,16 +36,10 @@ onBeforeMount(() => {
 
             <div class="flex justify-end w-full">
                 <div
-                    v-if="browserSuppport"
                     class="flex justify-end w-full my-auto"
                 >
-                    <CmpPusherState />
-                </div>
-                <div
-                    v-if="!browserSuppport"
-                    class="flex justify-end w-full my-auto"
-                >
-                    <button class="btn btn-sm btn-error">
+                    <CmpPusherState v-if="browserSuppport" />
+                    <button v-if="!browserSuppport" class="btn btn-sm btn-error">
                         <i class="pi pi-times m-1" />
                         <span class="m-1">Browser Unsupported</span>
                     </button>
