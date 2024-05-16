133e642600bf138ca67ab10dd641b3b8d526504a
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-05-16 09:38:05 +0000

    Removed Sentry

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,6 @@ const pinia: Pinia = createPinia();
 import PrimeVue from "primevue/config";
 import PrimeTailwind from "./presets/custom";
 
-import * as Sentry from "@sentry/capacitor";
-import * as SentrySibling from "@sentry/vue";
-
 import { IonicVue } from "@ionic/vue";
 
 /** Vue router needed for navigation menu */
@@ -25,30 +22,6 @@ import BaseApp from "./BaseApp.vue";
 import DialogService from "primevue/dialogservice";
 import ToastService from "primevue/toastservice";
 import Tooltip from "primevue/tooltip";
-
-/** Sentry iniitialization */
-Sentry.init(
-    {
-        dsn: import.meta.env.VITE_SENTRY_DSN,
-        release: process.env.npm_package_version,
-        dist: "1",
-        integrations: [
-            new SentrySibling.BrowserTracing({
-                // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-                tracePropagationTargets: [
-                    "localhost",
-                    import.meta.env.VITE_APP_HOST,
-                    import.meta.env.VITE_API_ENDPOINT,
-                ],
-                routingInstrumentation:
-                    SentrySibling.vueRouterInstrumentation(router),
-            }),
-        ],
-        // Performance Monitoring
-        tracesSampleRate: 0.01, //  Capture 10% of the transactions
-    },
-    SentrySibling.init,
-);
 
 // Mount Application Instances
 const MainApp: App<Element> = createApp(BaseApp)
