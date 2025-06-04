ca0b0e8d6cefec6565d5b0e7c58e46595d5bbf3e
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2025-06-04 16:27:22 +0700

    fix: update keystore file format to PKCS12 and adjust build.gradle for NDK version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.ntj125app.base_mobile"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
